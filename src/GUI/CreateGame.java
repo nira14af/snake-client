8fbc3c1eef1addac79858907773238bd03e6f8bc
nira14af <ni14af@student.cbs.dk>
2015-11-30 10:49:18 +0100

    CreateGame updated

--- src/GUI/CreateGame.java
+++ src/GUI/CreateGame.java
@@ -6,10 +6,14 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JTextField;
 
 public class CreateGame extends JPanel {
     private JButton btnMenu;
     private JButton btnCreateGame;
+    private JTextField textFieldCreateGame;
+    private JTextField textFietextFieldCreateGameld;
+    private JTextField textFieldMapSize;
 
     public CreateGame() {
 
@@ -27,12 +31,47 @@ public class CreateGame extends JPanel {
         add(btnMenu);
 
         btnCreateGame = new JButton("Create game");
-        btnCreateGame.setBounds(35, 84, 128, 23);
+        btnCreateGame.setBounds(33, 255, 128, 23);
         add(btnCreateGame);
 
         JLabel piclabel = new JLabel("");
         piclabel.setBounds(322, 13, 100, 91);
         add(piclabel);
+
+        JLabel lblGameName = new JLabel("Game Name");
+        lblGameName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        lblGameName.setBounds(33, 81, 96, 23);
+        add(lblGameName);
+
+        textFieldCreateGame = new JTextField();
+        textFieldCreateGame.setBounds(152, 76, 134, 28);
+        add(textFieldCreateGame);
+        textFieldCreateGame.setColumns(10);
+
+        JLabel lblControls = new JLabel("Controls");
+        lblControls.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        lblControls.setBounds(33, 134, 96, 23);
+        add(lblControls);
+
+        textFietextFieldCreateGameld = new JTextField();
+        textFietextFieldCreateGameld.setColumns(10);
+        textFietextFieldCreateGameld.setBounds(152, 132, 134, 28);
+        add(textFietextFieldCreateGameld);
+
+        JLabel lblChooseYourControls = new JLabel("Choose your controls (A, S, D W)");
+        lblChooseYourControls.setFont(new Font("Tahoma", Font.PLAIN, 9));
+        lblChooseYourControls.setBounds(293, 138, 167, 23);
+        add(lblChooseYourControls);
+
+        JLabel lblMapSize = new JLabel("Map size");
+        lblMapSize.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        lblMapSize.setBounds(33, 181, 96, 23);
+        add(lblMapSize);
+
+        textFieldMapSize = new JTextField();
+        textFieldMapSize.setColumns(10);
+        textFieldMapSize.setBounds(152, 179, 134, 28);
+        add(textFieldMapSize);
     }
 
     public void addActionListener(ActionListener l) {
@@ -46,5 +85,13 @@ public class CreateGame extends JPanel {
 
     public JButton getBtnCreateGame() {
         return btnCreateGame;
+    }
+
+    public JTextField getTextFieldCreateGame(){
+        return textFieldCreateGame;
+    }
+
+    public JTextField getTextFieldHostControls(){
+        return textFieldMapSize;
     }
 }
