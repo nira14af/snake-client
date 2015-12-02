cd3fd61fde5b64cd8a1495492f31efb422ce586b
nira14af <ni14af@student.cbs.dk>
2015-12-02 11:21:58 +0100

    StartGame gui changed to new layout with combobox

--- src/GUI/startGame.java
+++ src/GUI/startGame.java
@@ -2,38 +2,49 @@ package GUI;
 
 import java.awt.Font;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.Color;
 
 public class startGame extends JPanel {
+    private JLabel lblFindGame;
     private JButton btnMenu;
+    private JLabel lblSetGameDirections;
+    private JTextField directionsTextfield;
+    private JLabel lblChooseOpponent;
+    private JComboBox comboBox;
     private JButton btnStartGame;
 
     public startGame() {
-
-        setBackground(Color.LIGHT_GRAY);
         setLayout(null);
 
-        JLabel lbStartGame = new JLabel("Start Game");
-        lbStartGame.setFont(new Font("Tahoma", Font.BOLD, 14));
-        lbStartGame.setBounds(167, 35, 96, 23);
-        add(lbStartGame);
+        lblFindGame = new JLabel("Find game");
+        lblFindGame.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+        lblFindGame.setBounds(187, 6, 101, 16);
+        add(lblFindGame);
 
-        btnMenu = new JButton("Menu");
-        btnMenu.setToolTipText("");
-        btnMenu.setBounds(284, 230, 96, 23);
+        btnMenu = new JButton("Cancel");
+        btnMenu.setBounds(310, 240, 117, 29);
         add(btnMenu);
 
-        btnStartGame = new JButton("Start game");
-        btnStartGame.setBounds(58, 230, 96, 23);
-        add(btnStartGame);
+        lblChooseOpponent = new JLabel("1. Choose opponent");
+        lblChooseOpponent.setBounds(40, 70, 175, 16);
+        add(lblChooseOpponent);
 
-        JLabel piclabel = new JLabel("");
-        piclabel.setBounds(322, 13, 100, 91);
-        add(piclabel);
-    }
+        comboBox = new JComboBox();
+        comboBox.setBounds(40, 85, 52, 27);
+        add(comboBox);
+
+        lblSetGameDirections = new JLabel("2. Set directions (W,A,S,D)");
+        lblSetGameDirections.setBounds(40, 130, 175, 16);
+        add(lblSetGameDirections);
+
+        directionsTextfield = new JTextField();
+        directionsTextfield.setBounds(40, 150, 134, 28);
+        add(directionsTextfield);
+
+        btnStartGame = new JButton("Start game");
+        btnStartGame.setBounds(40, 200, 117, 29);
+        add(btnStartGame);}
 
     public void addActionListener(ActionListener l) {
         btnMenu.addActionListener(l);
