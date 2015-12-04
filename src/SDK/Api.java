1e902929612a1074838d3698515402651cc96965
nira14af <ni14af@student.cbs.dk>
2015-12-04 13:03:16 +0100

    Commit of changes in setup of login and createGame

--- src/SDK/Api.java
+++ src/SDK/Api.java
@@ -11,33 +11,91 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Api
-{
+public class Api {
 
+    ServerConnection serverConnection = new ServerConnection();
 
-        ServerConnection serverConnection = new ServerConnection();
+    public String login(User user) {
+        String data = serverConnection.post(new Gson().toJson(user), "login/");
 
-        public String login(User user)
-        {
+        JSONParser parser = new JSONParser();
 
-            String data = serverConnection.post(new Gson().toJson(user), "login/");
+        JSONObject jsonObject = null;
+        try {
+            Object object = parser.parse(data);
+            jsonObject = (JSONObject) object;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-            JSONParser parser = new JSONParser();
+        if (jsonObject != null) {
 
-            JSONObject jsonObject = null;
-            try
-            {
-                Object object = parser.parse(data);
-                jsonObject = (JSONObject) object;
-            } catch (ParseException e)
-            {
-                e.printStackTrace();
-            }
+            if (jsonObject.get("userid") != null)
+                user.setId((int) (long) jsonObject.get("userid"));
 
-            user.setId((int) (long) jsonObject.get("userid"));
+            return (String) jsonObject.get("message");
+        }
 
         return "";
+    }
+
+    public ArrayList<User> getUsers() {
+        String data = serverConnection.get("users/");
+        return new Gson().fromJson(data, new TypeToken<ArrayList<User>>() {
+        }.getType());
+    }
+
+    public String createGame(Game game) {
+        String data = serverConnection.post(new Gson().toJson(game), "games/");
+        HashMap<String, String> hashMap = new Gson().fromJson(data, HashMap.class);
+
+        return hashMap.get("message");
+    }
+
+    public ArrayList<Game> getGames(int userID) {
+        String data = serverConnection.get("games/pending/" + userID);
+        return new Gson().fromJson(data, new TypeToken<ArrayList<Game>>() {
+        }.getType());
+    }
+
+    public String joinGame(Game startGame) {
+        String data = serverConnection.put(new Gson().toJson(startGame), "games/join");
+        HashMap<String, String> hashMap = new Gson().fromJson(data, HashMap.class);
+
+        return hashMap.get("message");
+    }
+
+    public String startGame(Game startGame) {
+        String data = serverConnection.put(new Gson().toJson(startGame), "games/start");
+        HashMap<String, String> hashMap = new Gson().fromJson(data, HashMap.class);
+
+        if (hashMap.get("message") != null)
+            return hashMap.get("message");
+        else {
+            Game g = new Gson().fromJson(data, Game.class);
+            startGame.setWinner(g.getWinner());
+            System.out.println(g.getName());
+            return startGame.getWinner().getId()+ "";
         }
+    }
+
+    public ArrayList<Game> getGamesToDelete(int id) {
+        String data = serverConnection.get("games/host/" + id);
+        return new Gson().fromJson(data, new TypeToken<ArrayList<Game>>() {
+        }.getType());
+
+    }
+
+    public String deleteGame(int gameId) {
+        String data = serverConnection.delete("games/" + gameId);
+        HashMap<String, String> hashMap = new Gson().fromJson(data, HashMap.class);
+
+        return hashMap.get("message");
+    }
+
+    public ArrayList<Score> getHighscores() {
+        String data = serverConnection.get("Highscores/");
+        return new Gson().fromJson(data, new TypeToken<ArrayList<Score>>() {
+        }.getType());
+    }
 }
-
-
